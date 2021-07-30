fcc7fe2bd44de8bdc739dcc12f99e547d7245583
Daniel Serrano <ingm.daniel.serrano@gmail.com>
2021-07-30 17:06:11 -0500

    Funcion de login

--- models/task.model.js
+++ models/task.model.js
@@ -7,6 +7,7 @@ const TaskSchema = Schema({
     hourWeight      : { type: Number, requiered : true },
     hourStimation   : { type: Number, requiered : false },
     requirment      : { type: String, requiered : true },
+    currentState    : { type: String, requiered : false },
     description     : { type: String, requiered : false },
     finished        : { type: Boolean, default: false },
     success         : { type: Boolean, default: false },
